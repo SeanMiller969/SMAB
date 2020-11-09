ed87d2afb2251c9f79e4fcc7ec503897d2ac2860
macksjeremy <31860185+macksjeremy@users.noreply.github.com>
2020-11-09 14:52:45 -0500

    Better Printing

--- url_traversal.py
+++ url_traversal.py
@@ -18,18 +18,42 @@ def clean_json(url):
         for line in f:
             if(line != "\n"):
                 content.append(line.strip())
-    ###
     f.close()
-    #contentcopy = content
+    parsedoutput = []
+    parserpart = []
     print(len(content))
     for i in range(len(content)):
         print(content[i])
+        if(content[i] == "Starting at:"):
+            price1 = content[i + 1]
+            price2 = content[i + 2]
+            fullitem = content[i+3]
+            splititem = fullitem.split("|")
+            weapon = splititem[0]
+            splitseconditem = splititem[1].split("(")
+            print(splitseconditem)
+            skin = splitseconditem[0].strip()
+            condition = splitseconditem[1].replace(')', '')
+            print("Weapon is:", weapon)
+            print("Skin is:", skin)
+            print("Condition is:", condition)
+            print("Buy price is:", price1)
+            print("Sell price is:", price2)
+            parserpart.append(weapon)
+            parserpart.append(skin)
+            parserpart.append(condition)
+            parserpart.append(price1)
+            parserpart.append(price2)
+            print(parserpart)
+            parsedoutput.append(parserpart)
+            print(parsedoutput)
+            splititem.clear()
+            splitseconditem.clear()
 
-    #print(parsefile.read())
-    #for aline in parsefile:
-        #words = aline.split(' ')
-        #newlist = []
-        #for word in words:
-            #word = word.strip()
-            #newlist.append(word)
-        #print(newlist)
+    print(parsedoutput)
+    for i in parsedoutput:
+        print("Weapon is:", i[0])
+        print("Skin is:", i[1])
+        print("Condition is:", i[2])
+        print("Buy price is:", i[3])
+        print("Sell price is:", i[4])
