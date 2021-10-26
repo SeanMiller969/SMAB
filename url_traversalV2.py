0812c6c2d47be3b93aedd074414678939f9c8e29
john thiel <thiel.john@outlook.com>
2021-10-26 17:01:18 -0400

    Connected to Mongo DB

--- url_traversalV2.py
+++ url_traversalV2.py
@@ -1,7 +1,34 @@
 from bs4 import BeautifulSoup
 from urllib.request import urlopen
 from Weapon import Weapon
+import pymongo
 import ssl
+
+
+client = pymongo.MongoClient("mongodb+srv://thieljohn:SMABword123@cluster0.ehgdd.mongodb.net/SMAB?retryWrites=true&w=majority")
+db = client.SMAB
+weapon_col = db.Weapon
+
+def makeDoc(string_data, i):
+    buy_price = string_data[i + 1]
+    sell_price = string_data[i + 2]
+    full_listing = string_data[i + 3]
+    #Example listing --StatTrak™ Sawed-Off | Limelight (Well-Worn)
+    split_listing = full_listing.split("|")
+    #StatTrak checking implemented here ----
+    weapon = split_listing[0]
+    skin_condition = split_listing[1].split("(")
+    skin = skin_condition[0].strip()
+    condition = skin_condition[1].replace(')', '')
+    '''Should be updating prices at each weapon type not adding a new entry!!!!'''
+    weapon = {'gun' : weapon,
+        'skin' : skin,
+        'condition' : condition,
+        'buy_at' : buy_price,
+        'sell_at' : sell_price,
+    }
+
+
 
 def scrape_data(url):
     #read html data
@@ -20,19 +47,8 @@ def scrape_data(url):
                 lines.append(line.strip())
     f.close()
     #Now store the data in each line
-    print("check")
     for i in range(len(lines)):
         if lines[i] == "Starting at:":
-            #"starting at" is followed by our relevant data
-            buy_price = lines[i + 1]
-            sell_price = lines[i + 2]
-            full_listing = lines[i+3]
-            #Example listing --StatTrak™ Sawed-Off | Limelight (Well-Worn)
-            split_listing = full_listing.split("|")
-            #StatTrak checking implemented here ----
-            weapon = split_listing[0]
-            skin_condition = split_listing[1].split("(")
-            skin = skin_condition[0].strip()
-            condition = skin_condition[1].replace(')', '')
-            #Now lets put the weapons in Mongo under the case name
+            makeDoc(lines, i)
             """This means we need to connect to the MongoDB, possibly with Flask, not entirely sure."""
+            
