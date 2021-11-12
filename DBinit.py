7b607bfa29e032db3865c5a7ca4ef2b99271cbf7
john thiel <thiel.john@outlook.com>
2021-11-12 17:01:29 -0500

    Db initializing

--- DBinit.py
+++ DBinit.py
@@ -1,13 +1,33 @@
 # Initialize the bones of our MongoDB
 # call it a Smababase
-import pymongo
+from pymongo import MongoClient
 import pickle
 
-client = pymongo.MongoClient("mongodb+srv://thieljohn:SMABword123@cluster0.ehgdd.mongodb.net/SMAB?retryWrites=true&w=majority")
+#client = pymongo.MongoClient("mongodb+srv://thieljohn:SMABword1234@cluster0.ehgdd.mongodb.net/SMAB?retryWrites=true&w=majority")
+client = MongoClient('mongodb://localhost:2701')
 db = client.SMAB
-case_col = db.Case
 
 txt_file = pickle.load(open('setup/temp.pkl', 'rb'))
 for i in txt_file:
-    print(i)
+    #print(i)
+    if type(i) == str:
+        case_col = db[i]
+    else:
+        gun = i[1].split()[0]
+        skin = i[1].split("|")[1]
+        #print(i[1].split("|"))
+        rarity = i[0]
+        temp_dict = {
+            'Gun' : gun,
+            'Skin' : skin,
+            'Rarity' : rarity
+        }
+        case_col.insert_one(temp_dict)
+        print(temp_dict)
+
+print("Done!")
+
+    
+    
+    
 
